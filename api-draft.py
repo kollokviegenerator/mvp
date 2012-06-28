6e99b5326c64683045f20649b2bf8fa58d748b70
martinhb <martin.bielecki@gmail.com>
2012-06-28 16:13:34 +0200

    updated api

--- api-draft.py
+++ api-draft.py
@@ -8,17 +8,17 @@ class apidraft:
 
     def __init__(self):
         self.user_management = UserManagement()
-#        self.u = Unifisystem()
 
     def main(self, argv):
         """
-            banana banana banana
+            parse argv and execute the command(s)
         """
         try:
-            opts, args = getopt.getopt(argv[1:], "ha:", ["help", "adduser="])
+            opts, args = getopt.getopt(argv[1:],
+                "hu:g:", ["help", "adduser=", "du=", "deleteuser=", "flushusers"])
         except getopt.GetoptError, err:
             # print help information and exit:
-            print str(err) # will print something like "option -a not recognized"
+            print str(err) # will print something like "option -f not recognized"
             print "%s --help for usage" % sys.argv[0]
             sys.exit(2)
 
@@ -26,42 +26,66 @@ class apidraft:
             print "Remember the '--' before command!"
             sys.exit(1)
 
-
         for o, a in opts:
             if o in ("-h", "--help"):
                 self.usage()
                 sys.exit()
-            elif o in ("-a", "--adduser"):
+            elif o in ("-u", "--adduser"):
                 self.adduser(a)
+            elif o in ("-g", "--addgroup"):
+                print "adding group dummy text"
+            elif o in("--du", "--deleteuser"):
+                self.deleteuser(a)
+            elif o in("--flushusers"):
+                self.user_management.flush()
             else:
                 assert False, "unhandled option"
 
     def adduser(self, in_user):
 
-
-
+        """
+            Add a user to the database
+            @param in_user: the user to be added, can be a file or a single username
+        """
         try:
             #argument is a file of users
             f = open(in_user, 'r')
             for usr in f:
                 self.user_management.adduser(usr)
-        except:
+
+            f.close()
+        except IOError:
             #Argument is a single user
             self.user_management.adduser(in_user)
 
+    def deleteuser(self, in_user):
+        """
+            remove a user from the database
+            @param in_user: the user to delete
+        """
 
-    def usage():
+        try:
+            #argument is a file of users
+            f = open(in_user, 'r')
+            for usr in f:
+                self.user_management.deleteuser(usr)
+        except IOError:
+            self.user_management.deleteuser(in_user)
+
+
+    def usage(self):
         """
         Print usage
         """
 
-        print "Usage: %s [subcommand] [args] [options]\n" % sys.argv[0]
-        print "Options:"
+        print "Usage: %s [subcommand] [args] \n" % sys.argv[0]
+        print "Help:"
         print "    -h --help \tto show this help and exit\n"
-        print "Type '%s [subcommand] -h, --help for help on a specific subcommand\n"
+        print "Type '%s [subcommand] -h, --help for help on a specific subcommand (not impl. yet)\n"
         print "Available subcommands"
         print "[user]"
-        print "\t-a, --adduser"
+        print "\t-u, \t--adduser \t<user>"
+        print "\t--du, \t--deleteuser \t<user>"
 
 
 if __name__ == "__main__":
--- api/management/usermanagement.py
+++ api/management/usermanagement.py
@@ -5,23 +5,59 @@
 from mvp.models import Student, Oracle
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
+from django.db import transaction
 
 
 class UserManagement:
 
     """
-        banana banana banana
+        Takes care of user management (adding, removing, updating ...)
     """
     def __init__(self):
         pass
 
+    @transaction.commit_manually
     def adduser(self, usr):
-#        print usr
+        """
+            Add a user
+            @param usr: the user to be added
+        """
+        usr = usr.strip() #remove whitespace
         try:
             User.objects.create_user(usr)
+            print "user %s added!" % usr
+#            transaction.commit()
         except IntegrityError:
-            print "User '%s' exists!" % usr
+            print "User '%s' exists in database." % usr
+            transaction.rollback()
+            if not User.objects.get(username=usr).is_active:
+                print "If the user at some point was deleted "\
+                    "you want to do a user restore instead of add"
 
-#    def delete
+        finally:
+            transaction.commit()
 
+    def deleteuser(self, usr):
+        """
+            Delete a user (or rather, set the is_active flag to False so
+            any foreign keys to users won't break
+            @param usr: the user to remove
+        """
+
+    def update(self, usr, *args):
+        """
+            Update an user
+            @param usr: the user to update
+            @param: args: info to be updated
+        """
+
+        pass
+
+    def flush(self):
+        """
+            Removes all entries in the user table (and not just is_active=False)
+        """
+
+        User.objects.all().delete()
+        print "User table flushed"
 
