41b5b21ed6a77da4533ea20181362efadf611caf
ilyakh <ilyakh@ulrik.uio.no>
2012-07-03 14:22:18 +0200

    quadratic score for matching

--- match/util.py
+++ match/util.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 from __future__ import division
+from math import sqrt
 
 def all( a, b ):
     """
@@ -27,16 +28,24 @@ def wish_score( target, base ):
     """
     return len(common( target, base )) / len( target )
 
+def quadratic_score( a, b ):
+    return sqrt(wish_score(a,b) * wish_score(b,a))
 
 
 if __name__ == "__main__":
-    a = set(["a", "b", "c"])
-    b = set(["a", "b", "c", "d", "e"])
+    a = set(["a", "b", "c", "p"])
+    b = set(["a", "b", "c", "m", "p"])
 
-    print "Pair scores:"
-    print pair_score(a,b)
-    print pair_score(b,a)
-    print "(should be equal)"
+    a = set(["m"])
+    b = set(["a", "b", "c", "m", "p"])
+
     print "Wish scores:"
     print "a: %f" % wish_score(a,b)
     print "b: %f" % wish_score(b,a)
+    print "Pair scores:"
+    print " by wish pool"
+    print pair_score(a,b)
+    print pair_score(b,a)
+    print "(should be equal)"
+    print " comparison"
+    print "sqrt(a*b): %f" % ( quadratic_score(a,b) )
